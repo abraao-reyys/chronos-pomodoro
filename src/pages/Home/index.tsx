f77bf9a91c750827d30ec57d343673ad7bbee3d0
Abraão Reis <andradeabraao23@gmail.com>
2025-07-16 19:20:35 -0300

    data to create task simulator

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,19 +3,55 @@ import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { useTaskcontext } from '../../contexts/TaskContext/useTaskContext';
 import { MainTemplate } from '../../templates/MainTemplate';
+
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
 
 import styles from './styles.module.css';
 
 import { CirclePlayIcon } from 'lucide-react';
 
 export function Home() {
-  const { state } = useTaskcontext();
+  const { state, setState } = useTaskcontext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (taskNameInput === null) return;
+
+    const taskName = taskNameInput.current?.value.trim() ?? ''; // retorna '' se for null
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa!');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'pomodori',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, //conferir
+        secondsRemaining, //conferir
+        formattedSecondsRemaining: '00:00', //conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
+
   return (
     <MainTemplate>
       <div className={styles['main__content']}>
